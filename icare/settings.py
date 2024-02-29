074db3b812bafacb40784023c592d20dc2a7756f
Ha Le <ha.le.developer@protonmail.com>
2024-02-28 21:34:58 -0500

    Update settings.py

--- icare/settings.py
+++ icare/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ep1wi_9$mwu1+u2rdihpfis@zf)owjxv-n7vb%@u2jk*+3g)3_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
